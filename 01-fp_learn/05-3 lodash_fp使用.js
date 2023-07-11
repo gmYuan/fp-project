3d1016eb8af1097e83e14080998a45ca30e688cc
Ygm <ygm121351@163.com>
2023-07-11 23:10:27 +0800

    feat: fp的map

--- 01-fp_learn/05-3 lodash_fp使用.js
+++ 01-fp_learn/05-3 lodash_fp使用.js
@@ -10,3 +10,13 @@ console.log(fp.split(" ")("Hello World"));
 const f2 = fp.flowRight(fp.join("-"), fp.map(_.toLower), fp.split(" "));
 
 console.log("res是:", f2("NEVER SAY DIE"));
+
+
+// 例2: lodash 和 lodash/fp 模块中 map 方法的区别
+
+console.log('普通map值是--', _.map(['23', '8', '10'], parseInt))
+// parseInt('23', 0, array)
+// parseInt('8', 1, array)
+// parseInt('10', 2, array)
+
+console.log('fp map值是--', fp.map(parseInt, ['23', '8', '10']))
